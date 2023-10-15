980b1b6015b24064bc63e6f2707003f4311f11f3
Sejal <sejalpradhan290@gmail.com>
2023-10-15 20:00:49 +0530

    Updates

---
+++ precache-manifest.931fb34b31ece90a609141c1b5e619c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f64219ef195ef641d3e4",
+    "url": "/word-count/static/css/main.011be2db.chunk.css"
+  },
+  {
+    "revision": "f64219ef195ef641d3e4",
+    "url": "/word-count/static/js/main.f64219ef.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/word-count/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "cfb80bc6b254099f0163",
+    "url": "/word-count/static/js/runtime~main.cfb80bc6.js"
+  },
+  {
+    "revision": "e712c62db2ea4e65c0ad781d2cbd1193",
+    "url": "/word-count/index.html"
+  }
+];
